2a6fe7ed4c379fd019f55040ad5deab0fb3c1421
debojyoti paul <debojyoti11paul@gmail.com>
2018-09-11 02:39:04 +0530

    github commit

--- config/connect.js
+++ config/connect.js
@@ -1,10 +1,17 @@
 const mysql= require('mysql')
 
+//mysql://b547f5d25e28d7:a2455dd1@eu-cdbr-west-02.cleardb.net/heroku_de5142b7a03809f?reconnect=true
+
+
 let connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'todoapp'
+    host: 'eu-cdbr-west-02.cleardb.net',
+    user: 'b547f5d25e28d7',
+    password: 'a2455dd1',
+    database: 'heroku_de5142b7a03809f'
+});
+
+connection.connect((err)=>{
+    console.log("Error occurred while connecting to db")
 });
 
 module.exports = connection;
